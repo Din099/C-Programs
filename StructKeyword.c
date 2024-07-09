d4ce4f7f70cb365c84050f1fb7202c1c46cdd4ff
Dinesh <62383053+Din099@users.noreply.github.com>
2024-07-09 14:25:26 +0530

    Update StructKeyword.c
    using scanf() to take input

--- StructKeyword.c
+++ StructKeyword.c
@@ -9,7 +9,9 @@ struct phonebook{
 int main(void)
 {
     struct phonebook contact;
-    strcpy(contact.name,"Shiv");
+    scanf("%s",contact.name);
+    // printf("%s\n",contact.name);
+    //strcpy(contact.name,"Shiv");
     contact.phonenumber = 1234567890;
     printf("The name is %s and the number is %d",contact.name, contact.phonenumber);
     return 0;
