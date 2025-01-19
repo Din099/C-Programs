c928abcd22d889d2f91e6c2b692790118c12e669
Dinesh <62383053+Din099@users.noreply.github.com>
2025-01-19 21:03:11 +0530

    Create BitwiseNOR.c

---
+++ BitwiseNOR.c
@@ -0,0 +1,12 @@
+//Uses of bitwise NOR operator
+#include <stdio.h>
+
+int main()
+{
+    unsigned int x = 1;
+    //Output of the code is compiler dependent
+    printf("Signed int is %d\n", ~x);
+    printf("Unsigned int is %u\n", ~x);
+    
+    return 0;
+}
