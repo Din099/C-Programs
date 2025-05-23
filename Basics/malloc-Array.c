9e582ff72f018569ced3365585683076b489cfe7
Dinesh <62383053+Din099@users.noreply.github.com>
2025-05-24 00:04:03 +0530

    Create malloc-Array.c

---
+++ Basics/malloc-Array.c
@@ -0,0 +1,15 @@
+//Dynamic allocation of an array using pointer
+#include <stdio.h>
+#include<stdlib.h>
+int main(){
+    int *p;
+    p = (int*)malloc(5*sizeof(int));
+    
+    p[0]=10, p[1]=20, p[2]=30, p[3]=40, p[4]=50;
+    
+    for(int i=0; i<5; i++){
+        printf("%d\n",p[i]);
+    }
+    
+    return 0;
+}
